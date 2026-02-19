aeb8057ce2104660d545cdc975d6a3beda926f15
krishna kumar <krishnachaudhri861@gmail.com>
2026-02-19 10:05:36 +0530

    my first react commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <> this is a fragement
+    <div id='dad'> 
+      <h1 id='child1'>hello world</h1>
+      <h2 id='child2'>hello guys</h2> 
+      </div>
+      <div id='uncle'>
+        <h1 id='child3'>hii</h1>
+      </div>
+      </>
+  )
+}
+
+export default App
