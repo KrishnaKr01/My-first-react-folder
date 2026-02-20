6cbdd2d8edfd5216d739b8d8f19189edce80744a
krishna kumar <krishnachaudhri861@gmail.com>
2026-02-20 21:40:23 +0530

    Added 02 folder

---
+++ 02-folder/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+        <div className='card'>
+       <h1>hello my name is krishna chaudhary</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident dolores dolorum magnam distinctio vel eum voluptates deserunt dolor? Dolorem quia totam illo qui atque, placeat exercitationem commodi impedit ex nam.</p> 
+      </div>
+    </div>
+  )
+}
+
+export default card
